265010d481b9a45b1feafe490e86ffd6a5d1d60e
Venkatesh kumar <136155967+Venkateshkumardeveloper@users.noreply.github.com>
2024-07-27 12:26:14 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
     <div style={{display:"flex", justifyContent:"center", alignItems:"center", margin:"50px", background:"green"}}>App</div>
+    <p style={{display:"flex", justifyContent:"center", alignItems:"center", margin:"50px", background:"green"}}>Hello Team, How are you</p>
+    </>
   )
 }
 
